d46ad6d393acfa66f6cfbdaef87c6b3720112e4b
Maleesha Madhushanka <154691806+MaleeshaMAdhushanka@users.noreply.github.com>
2025-01-12 00:50:59 +0530

    Done Customer UpdateServlet

--- src/main/java/org/example/jsp/CustomerUpdateServlet.java
+++ src/main/java/org/example/jsp/CustomerUpdateServlet.java
@@ -1,8 +1,51 @@
 package org.example.jsp;
 
+import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 @WebServlet(name = "CustomerUpdateServlet", value ="/customer-update")
 public class CustomerUpdateServlet extends HttpServlet {
+    String DB_URL = "jdbc:mysql://localhost:3306/customerdb";
+    String DB_USER = "root";
+    String DB_PASSWORD = "Ijse@123";
+
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+      String id =   req.getParameter("id");
+      String name = req.getParameter("name");
+      String address = req.getParameter("address");
+      String email   =  req.getParameter("email");
+
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+         Connection connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+         String sql = "UPDATE customer SET name=?, address=?, email=? WHERE id=?";
+        PreparedStatement pstm = connection.prepareStatement(sql);
+        pstm.setString(1, name);
+        pstm.setString(2, address);
+        pstm.setString(3, email);
+        pstm.setString(4, id);
+        int updateRowCount =  pstm.executeUpdate();
+
+            if (updateRowCount > 0 ) {
+                resp.sendRedirect("customer-update.jsp?message=Customer Updated");
+            } else {
+                resp.sendRedirect("customer-update.jsp?error=Customer Not Updated");
+            }
+        } catch ( Exception e ) {
+            e.printStackTrace();
+            resp.sendRedirect("customer-update.jsp?error=Failed to update the customer");
+        }
+    }
 }
--- src/main/webapp/customer-update.jsp
+++ src/main/webapp/customer-update.jsp
@@ -8,9 +8,61 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
-    <title>Title</title>
+    <title>Customer Update</title>
 </head>
 <body>
+<h1>Update Customer</h1>
+
+<%
+   String message = (String) request.getParameter("message");
+   String error = (String) request.getParameter("error");
+%>
+<%
+    if (message != null){
+
+%>
+<div style="color: green">
+    <%= message %>
+</div>
+
+<%
+    }
+%>
+<%
+    if (error != null){
+%>
+<div style="color: red">
+    <%= error%>
+</div>
+<%
+    }
+%>
+
+<form action="customer-update" method="post">
+    <label for="id">ID:</label>
+    <input type="text" id="id" name="id"/>
+    <br/>
+    <br/>
+    <label for="name">Name:</label>
+    <input type="text" id="name" name="name"/>
+    <br/>
+    <br/>
+    <label for="address">Address:</label>
+    <input type="text" id="address" name="address"/>
+       <br/>
+    <br/>
+    <label for="email">Email:</label>
+    <input type="text" id="email" name="email"/>
+
+    <button type="submit" id="update">Update</button>
+
+
+
+
+
+
+
+</form>
 
 </body>
 </html>
